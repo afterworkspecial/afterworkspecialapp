d042e05a45d3b0ecdd1ca6e980a467bd45398f18
Olundir <krzysztof.jezak@outlook.com>
2022-09-12 21:50:34 +0200

    initial commit of files generated by Spring Initializer

---
+++ src/main/java/com/afterworkspecial/afterworkspecial/AfterworkspecialApplication.java
@@ -0,0 +1,13 @@
+package com.afterworkspecial.afterworkspecial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterworkspecialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfterworkspecialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afterworkspecial/afterworkspecial/AfterworkspecialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afterworkspecial.afterworkspecial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfterworkspecialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
