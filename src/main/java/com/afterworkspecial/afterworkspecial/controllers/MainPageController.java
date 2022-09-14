0aad5a561fd547576b26ca517f5ad05c11f42df4
Klimek Stoch <klimekstoch10@gmail.com>
2022-09-14 16:28:36 +0200

    Set controller and main.html file

--- src/main/java/com/afterworkspecial/afterworkspecial/controllers/MainPageController.java
+++ src/main/java/com/afterworkspecial/afterworkspecial/controllers/MainPageController.java
@@ -1,2 +1,13 @@
-package com.afterworkspecial.afterworkspecial.controllers;public class MainPageController {
+package com.afterworkspecial.afterworkspecial.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainPageController {
+
+    @GetMapping("/")
+    public String home() {
+        return "main";
+    }
 }
