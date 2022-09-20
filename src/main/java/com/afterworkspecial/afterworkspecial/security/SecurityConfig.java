1f9fde9d0011efdc70f9f93736f03e8e7fc05b00
Klimek Stoch <klimekstoch10@gmail.com>
2022-09-20 16:56:36 +0200

    Add logout - logging out thanks to /login

--- src/main/java/com/afterworkspecial/afterworkspecial/security/SecurityConfig.java
+++ src/main/java/com/afterworkspecial/afterworkspecial/security/SecurityConfig.java
@@ -1,16 +1,12 @@
 package com.afterworkspecial.afterworkspecial.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
@@ -20,7 +16,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .anyRequest().authenticated()
-                .and().formLogin().permitAll();
+                .and().formLogin().permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .httpBasic();
 //        If we'll creating own custom login page we have to add:
 //        .loginPage("/login")
     }
