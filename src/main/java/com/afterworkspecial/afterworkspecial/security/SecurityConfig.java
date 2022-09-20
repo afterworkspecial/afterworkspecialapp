6f1625fa723dc50b0e3dfa78e0d6b1e3d57d08dc
Klimek Stoch <klimekstoch10@gmail.com>
2022-09-20 16:21:46 +0200

    Add annotation to SecurityConfig and add inMemory user

--- src/main/java/com/afterworkspecial/afterworkspecial/security/SecurityConfig.java
+++ src/main/java/com/afterworkspecial/afterworkspecial/security/SecurityConfig.java
@@ -1,17 +1,38 @@
 package com.afterworkspecial.afterworkspecial.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .anyRequest().authenticated()
-                .and().formLogin()
-                .loginPage("/login").permitAll();
+                .and().formLogin().permitAll();
+//        If we'll creating own custom login page we have to add:
+//        .loginPage("/login")
+    }
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
