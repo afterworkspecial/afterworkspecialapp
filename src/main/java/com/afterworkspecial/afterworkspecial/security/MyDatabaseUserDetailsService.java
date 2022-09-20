cc391c2f3899c28b1b4142c7e4f94fb066cab157
Klimek Stoch <klimekstoch10@gmail.com>
2022-09-20 20:38:44 +0200

    Add UserDetailsService implementation

--- src/main/java/com/afterworkspecial/afterworkspecial/security/MyDatabaseUserDetailsService.java
+++ src/main/java/com/afterworkspecial/afterworkspecial/security/MyDatabaseUserDetailsService.java
@@ -1,2 +1,35 @@
-package com.afterworkspecial.afterworkspecial.security;public class MyDatabaseUserDetailsService {
+package com.afterworkspecial.afterworkspecial.security;
+
+import com.afterworkspecial.afterworkspecial.domain.User;
+import com.afterworkspecial.afterworkspecial.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class MyDatabaseUserDetailsService implements UserDetailsService {
+
+    UserRepository userRepository;
+
+    @Autowired
+    public MyDatabaseUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User dbUser =  userRepository.findAll().stream()
+                .filter(customer -> customer.getUsername().equals(username))
+                .findFirst()
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("Username %s not found", username)));
+
+        // Narazie nie mamy autoryzacji więc kolekcja jest pusta
+        return new org.springframework.security.core.userdetails.User(dbUser.getUsername(),
+                new BCryptPasswordEncoder().encode(dbUser.getPassword()), Collections.emptyList());
+    }
 }
