4c91585d7483577d9c32e7c7ba7c155df74a9ea6
Olundir <krzysztof.jezak@outlook.com>
2022-09-21 20:15:30 +0200

    added methods to Entities (cleaned up the Data annotation)

--- src/main/java/com/afterworkspecial/afterworkspecial/domain/Comment.java
+++ src/main/java/com/afterworkspecial/afterworkspecial/domain/Comment.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 import javax.persistence.*;
 
-@Data
 @Entity
 public class Comment {
 
@@ -14,4 +13,48 @@ public class Comment {
     private String text;
     @OneToOne
     private User user;
+
+    public Comment() {
+
+    }
+    public Comment(String text, User user) {
+        this.text = text;
+        this.user = user;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Comment)) return false;
+
+        Comment comment = (Comment) o;
+
+        if (id != null ? !id.equals(comment.id) : comment.id != null) return false;
+        if (text != null ? !text.equals(comment.text) : comment.text != null) return false;
+        return user != null ? user.equals(comment.user) : comment.user == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (text != null ? text.hashCode() : 0);
+        result = 31 * result + (user != null ? user.hashCode() : 0);
+        return result;
+    }
 }
--- src/main/java/com/afterworkspecial/afterworkspecial/domain/Whatsup.java
+++ src/main/java/com/afterworkspecial/afterworkspecial/domain/Whatsup.java
@@ -1,12 +1,9 @@
 package com.afterworkspecial.afterworkspecial.domain;
 
-import lombok.Data;
-
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Data
 @Entity(name = "whatsup")
 public class Whatsup {
 
@@ -20,4 +17,80 @@ public class Whatsup {
     private LocalDateTime createdDate;
     @OneToMany
     private List<Comment> commentsList;
+
+    public Whatsup() {
+
+    }
+    public Whatsup(User user, String text, int likes, LocalDateTime createdDate) {
+        this.user = user;
+        this.text = text;
+        this.likes = likes;
+        this.createdDate = createdDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public List<Comment> getCommentsList() {
+        return commentsList;
+    }
+
+    public void setCommentsList(List<Comment> commentsList) {
+        this.commentsList = commentsList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Whatsup)) return false;
+
+        Whatsup whatsup = (Whatsup) o;
+
+        if (likes != whatsup.likes) return false;
+        if (id != null ? !id.equals(whatsup.id) : whatsup.id != null) return false;
+        if (user != null ? !user.equals(whatsup.user) : whatsup.user != null) return false;
+        if (text != null ? !text.equals(whatsup.text) : whatsup.text != null) return false;
+        if (createdDate != null ? !createdDate.equals(whatsup.createdDate) : whatsup.createdDate != null) return false;
+        return commentsList != null ? commentsList.equals(whatsup.commentsList) : whatsup.commentsList == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (user != null ? user.hashCode() : 0);
+        result = 31 * result + (text != null ? text.hashCode() : 0);
+        result = 31 * result + likes;
+        result = 31 * result + (createdDate != null ? createdDate.hashCode() : 0);
+        result = 31 * result + (commentsList != null ? commentsList.hashCode() : 0);
+        return result;
+    }
 }
